98d686fbb1f5d722e43be715a676a242daeed731
andrzej-herman <aherman@san.edu.pl>
2023-07-24 13:40:20 +0200

    First Commit with project setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <>
+      <div className="text-6xl text-green-500">Hello Herman AI</div>
+      <div className="mt-2 ml-2">
+        <Button>Herman</Button>
+      </div>
+    </>
+  );
+};
+
+export default Home;
